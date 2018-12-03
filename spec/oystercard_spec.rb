9fda6c37d8816726984b9cef28c410c1972352f2
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-03 15:05:46 +0000

    Add the ability to update balance, all tests passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,13 @@ require 'oystercard'
 describe Oystercard do
 
   let (:card) { Oystercard.new }
- # This test is used in conjunction with ATTR_ACCESSOR to read the value of the vairable @balance. It checks the default value equals what we expect
+
   it 'has a starting balance of 0' do
-    expect(subject.balance).to eq(0)
+    expect(card.balance).to eq(0)
+  end
+#This test tests that a card with a default of 0, is then changed to 10 and then tests whether the output of the top_up method equals our expected mathmatical answer
+  it 'lets you top up your balance' do
+    card.balance = 10
+    expect(card.top_up(20)).to eq 30
   end
 end
