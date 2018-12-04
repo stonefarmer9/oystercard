6a8b87ba8d337bd3315422cbdbdb1655f5cb232f
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-04 15:50:42 +0000

    Add Charging functionality, all tests passed, one test(deduct) redundant & delete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,35 +1,32 @@
 class Oystercard
 
   MAX_BALANCE = 90
+  MINIMUM_FARE = 1
 
   attr_accessor :balance, :in_journey
 
-  def initialize(balance = 0)
-    @balance = balance
+  def initialize
+    @balance = 0
     @in_journey = false
   end
 
  def top_up(amount)
    max_balance_breach(amount)
-  # fail "#{MAX_BALANCE} limit breached" if amount + @balance > MAX_BALANCE
    @balance += amount
  end
 
- def deduct(amount)
-   @balance -= amount
- end
-
  def touch_in
+   fail "1" if @balance < MINIMUM_FARE
    @in_journey = true
  end
 
  def touch_out
+   deduct(MINIMUM_FARE)
    @in_journey = false
-
  end
 
 def in_journey
-  @in_journey = true
+  @in_journey
 end
 
 private
@@ -38,5 +35,12 @@ def max_balance_breach(amount)
   fail "#{MAX_BALANCE} limit breached" if amount + @balance > MAX_BALANCE
 end
 
+def deduct(amount)
+  @balance -= amount
+end
+
+def min_balance_breach(amount)
+
+end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,16 @@
 require 'oystercard'
+
 describe Oystercard do
 
   let (:card) { Oystercard.new }
+context '#balance' do
 
   it 'has a starting balance of 0' do
     expect(card.balance).to eq(0)
   end
+end
 
+context '#top_up' do
   it 'lets you top up your balance' do
     card.balance = 10
     expect(card.top_up(20)).to eq 30
@@ -17,23 +21,35 @@ describe Oystercard do
     card.top_up(maximum_balance)
     expect{ card.top_up 1 }.to raise_error "#{maximum_balance} limit breached"
   end
+end
 
-  it 'deducts an amount from the balance when used' do
-    card.balance = 30
-    expect(card.deduct(10)).to eq 20
-  end
+context '#touch_in' do
+
   it 'starts not checked in' do
+    card.top_up(1)
+    card.touch_in
     expect(card.in_journey).to be true
   end
+
   it 'Lets you touch-in and changes the status of the card' do
+    card.top_up(1)
     expect(card.touch_in).to be true
   end
 
+  it 'raises an error' do
+    expect { card.touch_in }.to raise_error "1"
+  end
+
+end
+
+context "#touch_out" do
+
   it 'Lets you touch-out and changes the status of the card' do
-
     expect(card.touch_out).to be false
+  end
+
+  it 'Deducts the MINIMUM_FARE from the balance on touch out' do
+    expect { card.touch_out }.to change{ card.balance }.by(-Oystercard::MINIMUM_FARE)
+  end
  end
-
-#####Step 8 mostly complete, needs final details finished
-
 end
