4d2d189158f3c462a914d92d0f10a0b38b865402
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-04 17:01:09 +0000

    Add entry station saving functionality, Fix tests that this broke, remove in_journey instance and all tests as no longer relevant or able to pass

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,11 +3,11 @@ class Oystercard
   MAX_BALANCE = 90
   MINIMUM_FARE = 1
 
-  attr_accessor :balance, :in_journey
+  attr_accessor :balance, :in_journey, :entry_station
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
   end
 
  def top_up(amount)
@@ -15,19 +15,15 @@ class Oystercard
    @balance += amount
  end
 
- def touch_in
+ def touch_in(station)
    fail "1" if @balance < MINIMUM_FARE
-   @in_journey = true
+   @entry_station = station
  end
 
  def touch_out
    deduct(MINIMUM_FARE)
-   @in_journey = false
+   @entry_station = nil
  end
-
-def in_journey
-  @in_journey
-end
 
 private
 
@@ -39,8 +35,7 @@ def deduct(amount)
   @balance -= amount
 end
 
-def min_balance_breach(amount)
-
+def in_journey?
+  !!@entry_station
 end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,10 @@ require 'oystercard'
 describe Oystercard do
 
   let (:card) { Oystercard.new }
+  let (:station) { double :station }
+
+
+
 context '#balance' do
 
   it 'has a starting balance of 0' do
@@ -25,31 +29,31 @@ end
 
 context '#touch_in' do
 
-  it 'starts not checked in' do
-    card.top_up(1)
-    card.touch_in
-    expect(card.in_journey).to be true
-  end
-
-  it 'Lets you touch-in and changes the status of the card' do
-    card.top_up(1)
-    expect(card.touch_in).to be true
-  end
-
   it 'raises an error' do
-    expect { card.touch_in }.to raise_error "1"
+    expect { card.touch_in(station) }.to raise_error "1"
+  end
+
+  it 'Saves the entry station' do
+    card.top_up(20)
+    card.touch_in(station)
+    expect(card.entry_station).to eq station
   end
 
 end
 
 context "#touch_out" do
 
-  it 'Lets you touch-out and changes the status of the card' do
-    expect(card.touch_out).to be false
-  end
 
   it 'Deducts the MINIMUM_FARE from the balance on touch out' do
     expect { card.touch_out }.to change{ card.balance }.by(-Oystercard::MINIMUM_FARE)
   end
+
+  it 'Touches out and deletes the entry station' do
+    card.top_up(20)
+    card.touch_in(station)
+    card.touch_out
+    expect(card.entry_station).to eq nil
+  end
+
  end
 end
